8c19b7ea259cd91617cc22e3eb595119fb379d55
cjain <chinmayjain2702@gmail.com>
2025-04-24 00:08:32 +0530

    50% work done - Only content need to be added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // Home route
+    { 
+      path: '', 
+      redirectTo:'home',
+      pathMatch:'full'
+    },//default route
+    { 
+        path: 'home', 
+        component: HomeComponent 
+    },// Home route
+    {
+        path:'aboutme',
+        component:AboutMeComponent
+    },
+    {
+        path:'projects',
+        component:ProjectComponent
+    },
+    {
+        path:'skills',
+        component:SkillsComponent
+    }
 ];
